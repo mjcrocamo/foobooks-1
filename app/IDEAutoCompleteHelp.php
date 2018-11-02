c08bffd2c8bc08dc7b177e77e070d3758982f0bf
Susan Buck <susan@susanbuck.net>
2018-11-02 11:32:42 -0400

    https://github.com/susanBuck/dwa15-fall2018/blob/master/laravel/laravel-and-phpstorm.md#resolve-validate-method-not-found-in-illuminatehttprequest-flag

---
+++ app/IDEAutoCompleteHelp.php
@@ -0,0 +1,10 @@
+<?php
+# https://github.com/barryvdh/laravel-ide-helper/issues/608
+namespace Illuminate\Http;
+
+/**
+ * @method bool validate(array $rules, ...$params) Validate the given request with the given rules.
+ */
+class Request
+{
+}
