925b4f60531c683d664c9053fcf715a24ad3d1dc
Susan Buck <susan@susanbuck.net>
2018-11-14 19:54:32 -0500

    Ide helper code; update composer; remove debug route

---
+++ _ide_helper_models.php
@@ -0,0 +1,35 @@
+<?php
+
+// @formatter:off
+/**
+ * A helper file for your Eloquent Models
+ * Copy the phpDocs from this file to the correct Model,
+ * And remove them from this file, to prevent double declarations.
+ *
+ * @author Barry vd. Heuvel <barryvdh@gmail.com>
+ */
+
+
+namespace App{
+/**
+ * App\Book
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Book newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Book newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Book query()
+ */
+	class Book extends \Eloquent {}
+}
+
+namespace App{
+/**
+ * App\User
+ *
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User query()
+ */
+	class User extends \Eloquent {}
+}
+
